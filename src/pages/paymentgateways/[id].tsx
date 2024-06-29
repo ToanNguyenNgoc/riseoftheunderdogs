10543e53d6fe276930ed245883e75e8fff5bf56f
Đỗ Cao Long <long@myspa.vn>
2024-06-29 09:55:03 +0700

    transation auth

--- src/pages/paymentgateways/[id].tsx
+++ src/pages/paymentgateways/[id].tsx
@@ -1,6 +1,6 @@
 import { GetServerSideProps } from 'next'
 import axios from 'axios'
-import { useEffect, useState } from 'react'
+import { baseURL } from '@/configs'
 
 interface PaymentProps {
   initialStatus: string
@@ -8,32 +8,16 @@ interface PaymentProps {
 }
 
 const PaymentStatus = ({ initialStatus, paymentId }: PaymentProps) => {
-  const [status, setStatus] = useState(initialStatus)
-
-  useEffect(() => {
-    const fetchStatus = async () => {
-      try {
-        const response = await axios.get(`https://riseoftheunderdogs.com/api/paymentgateways/${paymentId}`)
-        if (response.data && response.data.status) {
-          setStatus(response.data.status)
-        }
-      } catch (error) {
-        console.error('Error fetching payment status:', error)
-      }
-    }
-
-    fetchStatus()
-  }, [paymentId])
-
+  console.log(initialStatus)
   return (
     <div>
       <h1>Payment ID: {paymentId}</h1>
-      {status === 'PAID' ? (
+      {initialStatus === 'PAID' ? (
         <h2>Thanh toán thành công</h2>
-      ) : status === 'PENDING' ? (
+      ) : initialStatus === 'PENDING' ? (
         <h2>Đang chờ thanh toán</h2>
       ) : (
-        <h2>Sai</h2>
+        <h2>Hủy thanh toán</h2>
       )}
     </div>
   )
@@ -41,7 +25,7 @@ const PaymentStatus = ({ initialStatus, paymentId }: PaymentProps) => {
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const { id } = context.params as { id: string }
-
+  // console.log(id)
   if (!id) {
     return {
       notFound: true
@@ -51,10 +35,11 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
   let initialStatus = 'PENDING'
 
   try {
-    const response = await axios.get(`https://riseoftheunderdogs.com/api/paymentgateways/${id}`)
+    const response = await axios.get(`${baseURL}/api/paymentgateways/AadFqC`)
     if (response.data && response.data.status) {
-      initialStatus = response.data.status
+      initialStatus = response.data.context.status
     }
+    // console.log(response)
   } catch (error) {
     console.error('Error fetching payment status:', error)
   }
